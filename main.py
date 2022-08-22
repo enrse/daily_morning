32d5e9abf4f8f5cc1665429e1969b05c2bc0b7d9
enrse <49861865+enrse@users.noreply.github.com>
2022-08-22 10:09:57 +0800

    Update main.py
    OK

--- main.py
+++ main.py
@@ -17,14 +17,6 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
-print("start_date", start_date)
-print("city", city)
-print("birthday", birthday)
-print("app_id", app_id)
-print("app_secret", app_secret)
-print("user_id", user_id)
-print("template_id", template_id)
-
 
 def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
